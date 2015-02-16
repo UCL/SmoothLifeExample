8cb2bfef0e1a1a5d74b2851024b549253aba367c
James Hetherington <jamespjh@gmail.com>
2015-02-16 15:45:15 +0000

    Transition function definition

--- src/Smooth.cpp
+++ src/Smooth.cpp
@@ -5,18 +5,18 @@
 
 Smooth::Smooth(int size,
         distance inner,
-        filling birth_inner,
-        filling birth_outer,
-        filling death_inner,
-        filling death_outer,
+        filling birth_1,
+        filling birth_2 ,
+        filling death_1,
+        filling death_2,
         filling smoothing_inner,
         filling smoothing_outer)
     : size(100),
     inner(inner),
-    birth_inner(birth_inner),
-    birth_outer(birth_outer),
-    death_inner(death_inner),
-    death_outer(death_outer),
+    birth_1(birth_1),
+    birth_2(birth_2),
+    death_1(death_1),
+    death_2(death_2),
     smoothing_inner(smoothing_inner),
     smoothing_outer(smoothing_outer),
     outer(inner*3),
@@ -57,6 +57,12 @@ double Smooth::Ring(distance radius){
 
 double Smooth::Sigmoid(double center, double variable, double width){
   return 1.0/(1.0+std::exp(4.0*(center-variable)/width));
+}
+
+density Smooth::transition(filling inner, filling outer){
+  double t1=birth_1*(1.0-Sigmoid(inner,0.5,smoothing_inner))+death_1*Sigmoid(inner,0.5,smoothing_inner);
+  double t2=birth_2*(1.0-Sigmoid(inner,0.5,smoothing_inner))+death_2*Sigmoid(inner,0.5,smoothing_inner);
+  return Sigmoid(outer,t1,smoothing_outer)*(1.0-Sigmoid(outer,t2,smoothing_outer));
 }
 
 const std::vector<std::vector<density> > & Smooth::Field() const {
--- src/Smooth.h
+++ src/Smooth.h
@@ -8,10 +8,10 @@ class Smooth {
   public:
     Smooth(int size=100,
     distance inner=21.0,
-    filling birth_inner=0.278,
-    filling birth_outer=0.365,
-    filling death_inner=0.267,
-    filling death_outer=0.445,
+    filling birth_1=0.278,
+    filling birth_2=0.365,
+    filling death_1=0.267,
+    filling death_2=0.445,
     filling smoothing_inner=0.028,
     filling smoothing_outer=0.147);
     int Size();
@@ -19,14 +19,15 @@ class Smooth {
     double Disk(distance radius);
     double Ring(distance radius);
     static double Sigmoid(double centre, double variable, double width);
+    density transition(filling inner, filling outer);
   private:
     int size;
     std::vector<std::vector<density> > field;
     distance inner;
-    filling birth_inner;
-    filling birth_outer;
-    filling death_inner;
-    filling death_outer;
+    filling birth_1;
+    filling birth_2;
+    filling death_1;
+    filling death_2;
     filling smoothing_inner;
     filling smoothing_outer;
     distance outer;
