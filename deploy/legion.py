99fc2a8feaa5593ba47759577b1f334507b0288f
James Hetherington <jamespjh@gmail.com>
2016-03-01 17:22:33 +0000

    Update modules for new legion

--- deploy/legion.py
+++ deploy/legion.py
@@ -2,6 +2,7 @@ from fabric.api import *
 from mako.template import Template
 import mako
 import os
+from contextlib import nested
 
 env.run_at="/home/ucgajhe/Scratch/Smooth/output"
 env.deploy_to="/home/ucgajhe/devel/smooth"
@@ -9,34 +10,36 @@ env.clone_url="https://github.com/UCL/SmoothLifeExample.git"
 env.hosts=['legion.rc.ucl.ac.uk']
 env.user='ucgajhe'
 
+modules = nested(
+    prefix('module load cmake'),
+    prefix('module swap compilers compilers/gnu/4.9.2'),
+    prefix('module swap mpi mpi/openmpi/1.10.1/gnu-4.9.2')
+)
+
 @task
 def cold(branch='mpi'):
     run('rm -rf '+env.deploy_to)
     run('mkdir -p '+env.deploy_to)
     run('mkdir -p '+env.run_at)
     with cd(env.deploy_to):
-        with prefix('module load cmake'):
-            with prefix('module swap compilers compilers/gnu/4.6.3'):
-                with prefix('module swap mpi mpi/openmpi/1.6.5/gnu.4.6.3'):
-                    run('git clone '+env.clone_url)
-                    run('mkdir SmoothLifeExample/build')
-                    with cd('SmoothLifeExample/build'):
-                        run('git checkout '+branch)
-                        run('cmake .. -DCMAKE_CXX_COMPILER=mpiCC -DCMAKE_C_COMPILER=mpicc')
-                        run('make')
-                        run('test/catch')
+        with modules:
+            run('git clone '+env.clone_url)
+            run('mkdir SmoothLifeExample/build')
+            with cd('SmoothLifeExample/build'):
+                run('git checkout '+branch)
+                run('cmake .. -DCMAKE_CXX_COMPILER=mpiCC -DCMAKE_C_COMPILER=mpicc')
+                run('make')
+                run('test/catch')
 
 @task
 def warm(branch='mpi'):
   with cd(env.deploy_to+'/SmoothLifeExample/build'):
-        with prefix('module load cmake'):
-            with prefix('module swap compilers compilers/gnu/4.6.3'):
-                with prefix('module swap mpi mpi/openmpi/1.6.5/gnu.4.6.3'):
-                        run('git checkout '+branch)
-                        run('git pull')
-                        run('cmake ..')
-                        run('make')
-                        run('test/catch')
+        with modules:
+            run('git checkout '+branch)
+            run('git pull')
+            run('cmake ..')
+            run('make')
+            run('test/catch')
 
 @task
 def sub(processes=4):
